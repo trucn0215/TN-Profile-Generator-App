6dfad51d5d128d98da1a9c87aa3e2948ee8e944a
Truc Nguyen <trucn0215@gmail.com>
2020-12-11 01:22:10 -0800

    Render information and use fs to write html file

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const Choices = require("inquirer/lib/objects/choices");
 
 const employeeList = [];
 
@@ -78,7 +77,7 @@ function askUserForEmployeeType() {
 
         // ELSE
         else {
-            console.log(employeeList)
+            // console.log(employeeList)
             createHTML();
         }
     })
@@ -160,77 +159,15 @@ function createHTML() {
 
     const htmlContent = render(employeeList);
 
-    const createHtmlFile = [];
+    // console.log(htmlContent);
 
-    const htmlStart = `
-    <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>My Team</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
-        <script src="https://kit.fontawesome.com/c502137733.js"></script>
-    </head>
-    
-    <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 jumbotron mb-3 team-heading">
-                    <h1 class="text-center">My Team</h1>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="team-area col-12 d-flex justify-content-center">
-                `
-    createHtmlFile.push(htmlStart);
+    fs.writeFile(outputPath, htmlContent, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("HTML file created");
 
-    // ADD team members here
-
-    const htmlEnd = `
-                </div>
-            </div>
-        </div>
-    </body>
-    
-    </html>`;
-    createHtmlFile.push(htmlEnd);
-
-    // fs.writeFile("./output/team.html", html, function (err) {
-    //     if (err) {
-    //         console.log(err);
-    //     }
-    // });
-    console.log(createHtmlFile);
-    console.log("HTML file created");
+    });
 }
 
 askUserForManagerInfo(); // Use this to kick of the app
-
-// teamMember();
-// createHTML();
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
